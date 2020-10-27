aeda25fd79a556f12317bb1660c08027476486e9
yttr309 <63216289+yttr309@users.noreply.github.com>
2020-10-27 15:06:12 +0900

    3
    3

--- Assets/02.Scripts/LookItem.cs
+++ Assets/02.Scripts/LookItem.cs
@@ -5,6 +5,19 @@ using UnityEngine.EventSystems;
 
 public class LookItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IGvrPointerHoverHandler
 {
+
+    private Animator anim;
+    private readonly int hashIsOpen = Animator.StringToHash("IsOpen");
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void OnBoxOpen(bool isOpen)
+    {
+        anim.SetBool(hashIsOpen, isOpen);
+    }
+
     public void OnLookItemBox(bool isLookAt)
     {
         Debug.Log(isLookAt);
